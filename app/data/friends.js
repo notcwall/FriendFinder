dee7d3d60f2b6bd7a0cd656e6a5e92d34b6c0ddd
Conor Mullen <conorseamus@gmail.com>
2016-07-23 01:10:06 -0500

    working, needs modal, styling, and actual questions

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+	{
+		name: "John McFriendly",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Ryan_Gosling_-_Cannes_Film_Festival_-_01.jpg",
+		scores: [
+			3,
+			5,
+			1,
+			2,
+			2,
+			1,
+			5,
+			4,
+			5,
+			3
+		]
+	},
+	{
+		name: "Steven Notreallyachum",
+		photo: "http://vignette2.wikia.nocookie.net/villains/images/4/45/Joffrey-house-baratheon-29720605-1029-587.jpg/revision/latest?cb=20140418002909",
+		scores: [
+			1,
+			5,
+			5,
+			2,
+			1,
+			1,
+			3,
+			4,
+			5,
+			2
+		]
+	}
+]
+
+module.exports = friends;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,35 @@
+var path = require('path');
+
+var friends = require('../data/friends.js');
+
+function compareScores(inputUser){
+	var lowestDifference = 40;
+	var currentDifference = 0;
+	var bestMatch;
+
+	for(var i = 0; i < friends.length; i++){
+		if(inputUser.name != friends[i].name){
+			for(var j = 0; j < 10; j++){
+				currentDifference += Math.abs(inputUser.scores[j] - friends[i].scores[j]);
+			}
+			if(currentDifference < lowestDifference){
+				lowestDifference = currentDifference;
+				bestMatch = friends[i];
+			}
+			currentDifference = 0;
+		}
+	}
+
+	return bestMatch;
+}
+
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+	});
+
+	app.post('/api/friends', function(req, res){
+		friends.push(req.body);
+		res.send(compareScores(req.body));
+	});
+}
